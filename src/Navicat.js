542b9482a56d29a603e50b15403f7ef1c7a30ba1
dingqiang <dingqiang@axhome.com.cn>
2021-10-26 18:43:53 +0800

    加密解密，复制文本功能完成

--- src/Navicat.js
+++ src/Navicat.js
@@ -1,49 +1,82 @@
 import React from "react";
 import TextField from "@mui/material/TextField"
 import Container from "@mui/material/Container"
-import {Button} from "@mui/material"
-import Strings from "locutus/php/strings"
+import {Button, Grid} from "@mui/material"
 import CryptoJS from "crypto-js"
+import Strings from "locutus/php/strings"
+import CssBaseline from '@mui/material/CssBaseline';
 
 
 export default class Navicat extends React.Component {
+    state = {
+        aesKey: CryptoJS.enc.Utf8.parse("libcckeylibcckey"),
+        aesIv: CryptoJS.enc.Utf8.parse("libcciv libcciv "),
+        decodeTwelve: "",
+        encodeTwelve: "",
+    }
+
     constructor(props) {
         super(props);
-        this.state = {
-            aesKey: "libcckeylibcckey",
-            aesIv:"libcciv libcciv ",
-            decodeTweleve:"",
-        }
     }
-    handleChange = (event) => {
-        this.inputChange(event.target.value)
-    }
-    inputChange = (str) => {
-        this.encryptTweleve(str)
-    }
-    encryptEleven = (str) => {
 
+    handleChange = (event) => {
+        console.log(event.target.value);
+        this.encryptTwelve(event.target.value)
+        this.decryptTwelve(event.target.value)
     }
-    encryptTweleve = (str) => {
-        var decode = Strings.hex2bin(Strings.strtoupper(str))
-        this.state.decodeTweleve = CryptoJS.AES.decrypt(str, this.state.aesKey, {iv:this.state.aesIv})
+
+    // 加密
+    encryptTwelve = (str) => {
+        let options = {
+            iv: this.state.aesIv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        }
+        let encrypted = CryptoJS.AES.encrypt(str, this.state.aesKey, options)
+        let encryptedString = encrypted.toString();
+        let base64 = CryptoJS.enc.Base64.parse(encryptedString).toString().toUpperCase();
+        this.setState({
+            encodeTwelve: base64
+        })
+    }
+
+    // 解密
+    decryptTwelve = (str) => {
+        let low = Strings.strtolower(str)
+        let decode64 = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Hex.parse(low))
+        let decrypted = CryptoJS.AES.decrypt(decode64, this.state.aesKey, {
+            mode: CryptoJS.mode.CBC,
+            iv: this.state.aesIv,
+            padding: CryptoJS.pad.Pkcs7
+        });
+        let decryptedString = decrypted.toString(CryptoJS.enc.Utf8);
+        this.setState({
+            decodeTwelve: decryptedString
+        })
+    }
+
+    handleCopy = (value) => {
+        console.log('dd')
+        console.log(value)
+        if (typeof window.utools == 'object') {
+            window.utools.copyText(value)
+            window.utools.hideMainWindow()
+        }
     }
 
     render() {
-        const {label, value, index} = this.state
         return (
-            <div>
-                <Container maxWidth="sm">
-                    <TextField label="navicat密文" onChange={this.handleChange} variant="filled" fullWidth="true"/>
-                </Container>
-                <Container maxWidth="sm">
-                    <div>
-                        {{index}}
-                    </div>
-                    <Button></Button>
-                    <Button>复制</Button>
-                </Container>
-            </div>
-        );
+            <Container maxWidth="md">
+                <TextField label="navicat密文" onChange={this.handleChange} variant="filled" fullWidth="true"/>
+                <CssBaseline />
+                <Grid container spacing={1}>
+                    <Grid item md={12}>
+                        {this.state.decodeTwelve}---
+                        <Button onClick={() => this.handleCopy(this.state.decodeTwelve)}>点击复制</Button>
+                    </Grid>
+                </Grid>
+                <CssBaseline />
+            </Container>
+        )
     }
 }
